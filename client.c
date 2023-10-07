57d84c8acc9eeb0a818a0adffe5619ff60caf244
ISABELA GENIAL <isabelamgenial@gmail.com>
2023-10-07 01:58:38 -0300

    communication between client and server working.

--- client.c
+++ client.c
@@ -12,80 +12,56 @@
 
 #include "mini_talk.h"
 
-int send_bit(int pid, char *str);
-void sig_handler(int signum);
-int	ft_str_isdigit(char *c);
+int	send_bit(int pid, char *str);
+static int	ft_str_isdigit(char *c);
 
-int main (int argc, char **argv)
+int	main (int argc, char **argv)
 {
-    int num;
+    int					pid;
+	struct	sigaction	s_sig;
+	sigset_t			sigset;
 
     if (argc != 3 || !ft_str_isdigit(argv[1]))
     {
         ft_printf("ERROR: argument number");
         exit(EXIT_FAILURE);
     }
-	num = ft_atoi(argv[1]);
-    signal(SIGUSR1, sig_handler);
-    signal(SIGUSR2, sig_handler);
-
-    if(send_bit(num, argv[2]))
-    {
-        ft_printf("ERROR: signal no sent");
-        exit(EXIT_FAILURE);
-    }
-	
+	pid = ft_atoi(argv[1]);
+	sigaddset(&sigset, SIGUSR1);
+	sigaddset(&sigset, SIGUSR2);
+	s_sig.sa_handler = NULL;
+	s_sig.sa_flags = 0x0;
+	sigemptyset(&s_sig.sa_mask);
+	sigaction(SIGUSR1, &s_sig, NULL);
+	sigaction(SIGUSR2, &s_sig, NULL);
+	send_bit(pid, argv[2]);
 }
 
-int send_bit(int pid, char *str)
+int	send_bit(int pid, char *str)
 {
     char message;
-    int bit;
+    static int bit;
 
     while (*str)
     {
         message = *str;
-        bit = 7;
-        while (bit >= 0)
+        bit = 0;
+        while (bit < 8)
         {
-            if ((message >> bit) & 1)
-            {
-                if(kill(pid, SIGUSR2) == -1)
-                    return(1);
-            }
+			if ((0x80 >> bit) & message)
+            	kill(pid, SIGUSR1);
             else
-            {
-               if (kill(pid, SIGUSR1) == -1)
-                   return(1);
-            }
-            bit--;
+            	kill(pid, SIGUSR2);
+			usleep(500);
+            bit++;
         }
         str++;
     }
     return (0);
 }
 
-void sig_handler(int signum)
-{
-    int end;
 
-    end = 0;
-    if (signum == SIGUSR1)
-            end = send_bit(0, 0);
-    else if (signum == SIGUSR2)
-    {
-        ft_printf("ERROR: server ended unexpectdly.\n");
-        exit(EXIT_FAILURE);
-    }
-    if (end)
-    {
-        ft_printf("CLIENT: operation sucessful.\n");
-        exit(EXIT_SUCCESS);
-    }
-
-}
-
-int	ft_str_isdigit(char *c)
+static int	ft_str_isdigit(char *c)
 {
     while (*c)
     {
--- server.c
+++ server.c
@@ -12,20 +12,49 @@
 
 #include "mini_talk.h"
 
+void handler(int signum, siginfo_t *info, void *context);
 
-void handle_signal (int signal)
+int main (void)
 {
-	ft_printf("%d", signal);
+	int					pid;
+	struct sigaction	s_sig;
+	sigset_t			sigset;
+
+	sigemptyset(&sigset);
+	sigaddset(&sigset, SIGUSR1);
+	sigaddset(&sigset, SIGUSR2);
+	s_sig.sa_handler = NULL;
+	s_sig.sa_flags = SA_SIGINFO;
+	s_sig.sa_mask = sigset;
+	s_sig.sa_sigaction = handler;
+	sigaction(SIGUSR1, &s_sig, NULL);
+	sigaction(SIGUSR2, &s_sig, NULL);
+	pid = getpid();
+	ft_printf("pid %i\n", pid);
+	while (369)
+		pause();
 }
 
-int main ()
+void handler(int signum, siginfo_t *info, void *context)
 {
-	int			pid;
+	static unsigned char	c = 0x0;
+	static int				bit = 0;
+	int						pid;
 
-	pid = getpid();
-	signal(SIGUSR1, handle_signal);
-	ft_printf("pid %i\n", pid);
-	while (1)
-		pause();
-	return (0);
+	(void)context;
+	if(info->si_pid)
+		pid = info->si_pid;
+	if (signum == SIGUSR1)
+			c |= 0x1;
+	if (++bit == 8)
+	{
+		write(1, &c, 1);
+		c = 0x0;
+		bit = 0;
+	}
+	c <<= 0x1;
+//	if (signum == SIGUSR1)
+//		kill(info->si_pid, SIGUSR1);
+//	else
+//		kill(info->si_pid, SIGUSR2);
 }
